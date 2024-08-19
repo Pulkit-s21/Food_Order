4d62ef8741d2f37272930cd1d0a57186be84c69c
Pulkit-s21 <pulkitsaxena795@gmail.com>
2024-08-19 12:23:55 +0530

    Misc

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import "./App.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { Login } from "./pages/Login"
 import { Signup } from "./pages/Signup"
 import { Home } from "./pages/Home"
